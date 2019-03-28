455ee9448b0d3510f7e035fff8d2097e31c410bc
Ruslan Arhypenko <ruslanarhypenko@ruslans-macbook-air.local>
2019-03-28 15:45:57 +0100

    Initial Commit

---
+++ Simple Names/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Simple Names
+//
+//  Created by Ruslan Arhypenko on 3/28/19.
+//  Copyright © 2019 Ruslan Arhypenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
